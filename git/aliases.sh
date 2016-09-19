da945a383a78128c9786036b7760cba3bb45e33a
Alex Wu <wuyiding@gmail.com>
2016-09-19 10:33:03 +0800

    git alias

---
+++ git/aliases.sh
@@ -0,0 +1,12 @@
+
+# git command  aliases
+
+alias gl='git pull --prune'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+# Push local repo's HEAD ref to default remote origin 
+# the master is the branch name
+alias gp='git push origin HEAD'
+
+
+
+
